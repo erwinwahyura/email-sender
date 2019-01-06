29576447ac74a2fd4c3f05aeb00d014949fb41d2
erwinwahyura <erwinwahyuramadhan@gmail.com>
2019-01-06 21:09:36 +0700

    add send whatsapp message using twillio

---
+++ app/lib/twillio-wa.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken);
+
+// 1. appointment reminders
+client.messages
+      .create({
+         body: 'Your appointment is coming up on July 21 at 3PM',
+         from: 'whatsapp:+14155238886',
+         to: 'whatsapp:+6282242361317'
+       })
+      .then(message => console.log(message.sid))
+      .done();
+
+// 2. order notification
+const accountSid = process.env.ACCOUNT_SID
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+         body: 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',
+         from: 'whatsapp:+14155238886',
+         to: 'whatsapp:+6282242361317'
+       })
+      .then(message => console.log(message.sid))
+      .done();
